542006753119d4fae745dfe3555ccb086b4b12d6
Muffakin <alan.michael.degroot@hotmail.com>
2017-11-19 19:36:07 -0500

    Javascript closes modal to allow data submission while closing modal. Update ascii font to use Courier New or a Monospace.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Art.create(title: 'Kirby', artwork: '<(._.<) <(._.)> (>._.)>', artist: 'AJ')
+Art.create(title: 'Flip', artwork: '(╯°□°）╯︵ ┻━┻', artist: 'Angry Man')
+Art.create(title: 'Cutie Patootie Bear', artwork: 'ʕ•ᴥ•ʔ', artist: 'Bear with a Crush <3')
+Art.create(title: 'Fuck It', artwork: 't(-_-t)', artist: 'Everyone Every Monday')
+Art.create(title: 'Hugger', artwork: '(⊃｡•́‿•̀｡)⊃', artist: 'Close Friend')
+Art.create(title: 'Zombie', artwork: '[¬º-°]¬', artist: 'Necromancer')
